81799659e30397a911fb386b3b7d4934f3d89cb6
LeonelAnadon <leonelanadon95@gmail.com>
2022-04-23 19:35:29 -0300

    last commit 1.0v

--- src/api/api.js
+++ src/api/api.js
@@ -2,6 +2,5 @@ import axios from "axios";
 
 
 export default axios.create({
-    // baseURL: 'http://192.168.100.2:3939'
-    baseURL: 'http://179.43.127.167/'
+    baseURL: 'http://192.168.100.2:3939'
 })
